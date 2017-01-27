12d5efc0dbe57446361c92e28c286c201e2e0176
klumsieklutz <cheng916@scc2102-8.ucdavis.edu>
2017-01-26 16:28:29 -0800

    Add studio 2

---
+++ studio2/script.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+console.log("reading");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+
+    // all other js here
+
+    //capture the submit event
+    document.f.onsubmit = processForm;
+
+    //define process function
+    function processForm() {
+
+      console.log('processing');
+
+        //store inputs in variable
+        var food = document.f.food.value;
+        var plant = document.f.plant.value;
+        var adj = document.f.adj.value;
+        var animal = document.f.animal.value;
+        var ingVerb = document.f.ingVerb.value;
+
+        //call a new message to concatenate a message with inputs
+        var myMsg = document.getElementById("myMsg");
+        console.log(food);
+
+        myMsg.innerHTML = "Somebody once told me the world is a " +  food + ". So I took a bite out of a " +  plant + ". It tasted kind of " + adj + ". So I spitted it on a " + animal + ", and the " + animal + " started " + ingVerb + " on me";
+
+      return false;
+      }
+
+
+
+});
